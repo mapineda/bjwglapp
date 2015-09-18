5fce8a955a03c815a7b462aa7132ee25ba2662a0
mapineda Pineda <mapineda1222@gmail.com>
2015-09-18 00:21:03 -0500

    adding a planet

--- starter/public/js/app.js
+++ starter/public/js/app.js
@@ -12,75 +12,25 @@ BjsApp.init = function(){
 	//create a new scene
 	var scene = new BABYLON.Scene(engine);
 
+	//create a camera
+	var camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 15, BABYLON.Vector3.Zero(), scene);
 
-	//create camera
-	var camera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0 ,2, -15), scene);	
-	// interactive camera
+	//let the user move the camera
 	camera.attachControl(canvas);
 
-	// light environment light (comes from above)
-	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+	//light
+	var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
 
-	// ground mesh 
-	var ground = BABYLON.Mesh.CreateGround('ground1', 20, 20, 2, scene);
-	// grass mesh 
-	var grass = new BABYLON.StandardMaterial('grass', scene);
-	grass.diffuseTexture = new BABYLON.Texture('assets/images/grass.png', scene);
-	grass.diffuseTexture.uScale = 10;
-	grass.diffuseTexture.vScale = 10;
-	grass.specularColor = new BABYLON.Color3(0,0,0);
+	//sun
+	var sun = BABYLON.Mesh.CreateSphere('sun', 16, 4, scene);
 
+	//planets
+	var planetMaterial = new BABYLON.StandardMaterial('planetMat', scene);
+	planetMaterial.diffuseTexture = new BABYLON.Texture('public/assets/images/sand.png', scene);
 
-	ground.material = grass;
-
-
-	//create a sphere
-	var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-	//sphere position
-	sphere.position.y = 1;
-
-	//second sphere
-	var sphere2 = BABYLON.Mesh.CreateSphere('sphere2', 16, 4, scene);
-	//sphere 2 position`
-	sphere2.position = new BABYLON.Vector3(3, 3, 3);
-	sphere2.scaling = new BABYLON.Vector3(1, 0.5, 0.5);
-
-	var sphereMaterial = new BABYLON.StandardMaterial('sphereMat', scene);
-	// adding color red
-	sphereMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0);
-	// add transperancy
-	sphereMaterial.alpha = 0.5;
-	sphereMaterial.specularColor = new BABYLON.Color3(0,0,1);
-	sphereMaterial.specularPower = 5;
-
-	sphere2.material = sphereMaterial;
-
-	// create a box
-	var box = BABYLON.Mesh.CreateBox('box', 1, scene);
-	box.position = new BABYLON.Vector3(5, 2, -5);
-	box.scaling.y = 2;
-
-	//make box rotate
-	box.rotation.x = 45;
-
-	box.material = sphereMaterial;
-
-	//create a cylinder
-	var cylinder = BABYLON.Mesh.CreateCylinder('cyl', 5, 1, 3, 30, scene);
-	//emit the cone in the night time
-	var emissiveMaterial = new BABYLON.StandardMaterial('emissiveMat', scene);
-	emissiveMaterial.emissiveColor = new BABYLON.Color3(0,1,0);
-
-	cylinder.material = emissiveMaterial;
-
-	// create lines 
-	var lines = BABYLON.Mesh.CreateLines('lines', [
-			new BABYLON.Vector3(0, 5, 0), 
-			new BABYLON.Vector3(1, 5, 0), 
-			new BABYLON.Vector3(0, 5, 1), 
-			new BABYLON.Vector3(2, 2, 0), 
-			new BABYLON.Vector3(1, 5, -5)
-			], scene );
+	var planet1 = BABYLON.Mesh.CreateSphere('planet1', 16, 1, scene);
+	planet1.position.x = 4;
+	planet1.material = planetMaterial;
 
 	//render the scene
 	engine.runRenderLoop(function(){
